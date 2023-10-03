4416b321b0e608321465b5be01b8fa09b9b01769
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-03 20:42:57 +0300

    Доработка класса содержания архива

---
+++ OTIK_MIET/ArchiveData.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace OTIK_MIET;
+
+[Serializable]
+public class ArchiveData
+{
+	public ArchiveHeader Header { get; set; }
+	public List<ArchiveFile> Files { get; set; } // Список файлов
+
+	public ArchiveData()
+	{
+		Header = new ArchiveHeader();
+		Files = new List<ArchiveFile>();
+	}
+
+	// Сериализация объекта в бинарный файл
+	public static void SerializeToBinaryFile(string filePath, object obj)
+	{
+		using (FileStream fs = new FileStream(filePath, FileMode.Create))
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			formatter.Serialize(fs, obj);
+		}
+	}
+
+	// Десериализация объекта из бинарного файла
+	public static object DeserializeFromBinaryFile(string filePath)
+	{
+		using (FileStream fs = new FileStream(filePath, FileMode.Open))
+		{
+			BinaryFormatter formatter = new BinaryFormatter();
+			return formatter.Deserialize(fs);
+		}
+	}
+}
+
