86f2b7894ad50d8321c9c3a2cfc0a97988024788
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-03 16:47:44 +0300

    Создание файла

--- OTIK_MIET/Program.cs
+++ OTIK_MIET/Program.cs
@@ -1,6 +1,16 @@
 ﻿using OTIK_MIET;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
 
 // Создание архива
 Archive archive = new Archive();
 
 byte[] archiveBytes;
+using (MemoryStream stream = new MemoryStream())
+{
+	IFormatter formatter = new BinaryFormatter();
+	formatter.Serialize(stream, archive);
+	archiveBytes = stream.ToArray();
+}
+
+File.WriteAllBytes("C:\\Users\\pin11\\source\\repos\\OTIK_MIET\\OTIK_MIET\\myarchive.arc", archiveBytes);
