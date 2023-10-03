3da60c02417b4f46ad8ee503ac81cee8a5af7f7d
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-03 20:43:23 +0300

    Доработка архивного заголовка

--- OTIK_MIET/ArchiveHeader.cs
+++ OTIK_MIET/ArchiveHeader.cs
@@ -5,17 +5,17 @@ public class ArchiveHeader
 {
 	public string Signature { get; set; } // Сигнатура
 	public int Version { get; set; } // Версия формата
-	public int CompressionAlgorithmCode { get; set; } // Код алгоритма сжатия
-	public int ErrorProtectionCode { get; set; }  // Код алгоритма защиты
-	public long OriginalFileSize { get; set; }  // Исходная длина файла (байты)
+	public List<string> CompressionAlgorithms { get; set; } // Коды алгоритмов сжатия
+	public List<string> ErrorProtectionAlgorithms { get; set; } // Коды алгоритмов защиты от помех
+	public long OriginalFileSize { get; set; } // Исходная длина файла (в байтах)
 
-	// Инициализация по умолчанию
 	public ArchiveHeader()
 	{
 		Signature = "ARCV";
 		Version = 1;
-		CompressionAlgorithmCode = 1;
-		ErrorProtectionCode = 2;
+		CompressionAlgorithms = new List<string> { "LZW", "Huffman" };
+		ErrorProtectionAlgorithms = new List<string> { "ReedSolomon" };
 		OriginalFileSize = 1024;
 	}
 }
+
