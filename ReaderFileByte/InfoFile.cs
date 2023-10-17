e2d8c72b900a6700ab934ccaa4a67b93ce586114
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-17 17:04:40 +0300

    Считывание файла в байтах

---
+++ ReaderFileByte/InfoFile.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace ReaderFileByte;
+
+public class InfoFile
+{
+	public string FilePath { get; set; } = null!;
+
+	public Dictionary<byte, int> byteFrequency = new();
+
+	public int TotalCharacters { get; set; }
+
+	public double TotalInformation { get; set; }
+
+	public InfoFile(string filePath)
+	{
+		FilePath = filePath;
+		TotalInformation = 0;
+	}
+
+	public void CharacterCouting()
+	{
+		byte[] content = File.ReadAllBytes(FilePath);
+		TotalCharacters = content.Length;
+
+		PrintInfo.PrintCountCharacters(TotalCharacters);
+	}
+
+	public void FrequencyCouting()
+	{
+		byte[] content = File.ReadAllBytes(FilePath);
+
+		foreach (byte c in content)
+		{
+			if (byteFrequency.ContainsKey(c))
+				byteFrequency[c]++;
+			else
+				byteFrequency[c] = 1;
+		}
+
+		var sortedAlphabetically = byteFrequency.OrderBy(pair => pair.Key);
+		var sortedByFrequency = byteFrequency.OrderByDescending(pair => pair.Value);
+
+		Console.WriteLine("Таблица символов (отсортированная по алфавиту):");
+		PrintInfo.PrintCharacterTable(sortedAlphabetically);
+
+		Console.WriteLine("\nТаблица символов (отсортированная по убыванию частоты):");
+		PrintInfo.PrintCharacterTable(sortedByFrequency);
+	}
+
+	// Расчет вероятностей, количества информации и суммарной информации
+	public void InformationCouting()
+	{
+		foreach (var pair in byteFrequency)
+		{
+			double probability = (double)pair.Value / TotalCharacters;
+			double information = -Math.Log2(probability);
+			TotalInformation += information;
+			Console.WriteLine($"\nСимвол: {pair.Key}, Вероятность: {probability}, Количество информации: {information} бит");
+		}
+
+		double totalInformationBytes = TotalInformation / 8;
+		Console.WriteLine($"\nСуммарное количество информации в битах: {TotalInformation}");
+		Console.WriteLine($"Суммарное количество информации в байтах: {totalInformationBytes}");
+	}
+
+}
---
+++ ReaderFileByte/PrintInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ReaderFileByte;
+
+public class PrintInfo
+{
+	public static void PrintCharacterTable(IEnumerable<KeyValuePair<byte, int>> charFrequencies)
+	{
+		Console.WriteLine("Символ\tЧастота");
+		foreach (var pair in charFrequencies)
+		{
+			Console.WriteLine($"{pair.Key}\t{pair.Value}");
+		}
+	}
+
+	public static void PrintCountCharacters(int totalCharacters)
+	{
+		Console.WriteLine($"Длина файла в символах: {totalCharacters}");
+	}
+}
