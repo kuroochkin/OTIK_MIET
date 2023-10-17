9aa739ad7a0d739443be0263f7687b7d8a4c1339
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-17 17:04:52 +0300

    Считывание файла в Unicode

---
+++ ReaderFile/InfoFile.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace ReaderFile;
+
+public class InfoFile
+{
+	public string FilePath { get; set; } = null!;
+
+	public Dictionary<char, int> charFrequencies = new();
+
+	public int TotalCharacters { get; set; }
+
+	public double TotalInformation { get; set; }
+
+	public InfoFile(string filePath)
+	{
+		FilePath = filePath;
+		TotalInformation = 0;
+	}
+
+	public void CharacterCouting()
+	{
+		string content = File.ReadAllText(FilePath);
+		TotalCharacters = content.Length;
+
+		PrintInfo.PrintCountCharacters(TotalCharacters);
+	}
+
+	public void FrequencyCouting()
+	{
+		string content = File.ReadAllText(FilePath, Encoding.UTF8);
+
+		foreach (char c in content)
+		{
+			if (charFrequencies.ContainsKey(c))
+				charFrequencies[c]++;
+			else
+				charFrequencies[c] = 1;
+		}
+
+		var sortedAlphabetically = charFrequencies.OrderBy(pair => pair.Key);
+		var sortedByFrequency = charFrequencies.OrderByDescending(pair => pair.Value);
+
+		Console.WriteLine("Таблица символов (отсортированная по алфавиту):");
+		PrintInfo.PrintCharacterTable(sortedAlphabetically);
+
+		Console.WriteLine("\nТаблица символов (отсортированная по убыванию частоты):");
+		PrintInfo.PrintCharacterTable(sortedByFrequency);
+	}
+
+	// Расчет вероятностей, количества информации и суммарной информации
+	public void InformationCouting()
+	{
+		foreach (var pair in charFrequencies)
+		{
+			double probability = (double)pair.Value / TotalCharacters;
+			double information = -Math.Log2(probability);
+			TotalInformation += information;
+			Console.WriteLine($"\nСимвол: {pair.Key}, Вероятность: {probability}, Количество информации: {information} бит");
+		}
+
+		double totalInformationBytes = TotalInformation / 8;
+		Console.WriteLine($"\nСуммарное количество информации в битах: {TotalInformation}");
+		Console.WriteLine($"Суммарное количество информации в байтах: {totalInformationBytes}");
+	}
+
+
+}
+
---
+++ ReaderFile/PrintInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace ReaderFile;
+
+public static class PrintInfo
+{
+	public static void PrintCharacterTable(IEnumerable<KeyValuePair<char, int>> charFrequencies)
+	{
+		Console.WriteLine("Символ\tЧастота");
+		foreach (var pair in charFrequencies)
+		{
+			Console.WriteLine($"{pair.Key}\t{pair.Value}");
+		}
+	}
+
+	public static void PrintCountCharacters(int totalCharacters)
+	{
+		Console.WriteLine($"Длина файла в символах: {totalCharacters}");
+	}
+}
